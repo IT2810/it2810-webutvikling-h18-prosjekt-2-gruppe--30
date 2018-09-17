dcfce5ced64f1104c62b416486103c787c63d01d
olekfur <olekfur@gmail.com>
2018-09-17 10:34:28 +0200

    fix play correct sound by prop from app

--- utstilling/src/LoadSound.js
+++ utstilling/src/LoadSound.js
@@ -1,28 +1,55 @@
 import React from 'react';
-import './Sound/seagull.mp3';
 
 
 class LoadSound extends React.Component {
-  constructor() {
-    super();
-    this.state = {selectedSound: "seagull"}
-    this.playAudio = () => {
-      switch(this.state.selectedSound) {
-        case "seagull":
-          this.seagull.play();
-        default:
-          this.seagull.play();
-      }
+  constructor(props) {
+    super(props);
+    this.state = {selectedSound: 3}
+    this.playSound = this.playSound.bind(this);
+    this.render = this.render.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      selectedSound: this.props.caseindex
+    })
+  }
+
+  playSound() {
+
+    console.log(this.props.caseindex);
+    console.log(this.state);
+    console.log(this.state.selectedSound);
+    switch(this.state.selectedSound) {
+      case 1:
+        this.one.play();
+        break;
+      case 2:
+        this.two.play();
+        break;
+      case 3:
+        this.three.play()
+        break;
     }
   }
 
   render() {
     return (
       <div>
-        <audio ref={(seagull) => {this.seagull = seagull;}}>
+        <audio ref={(one) => {this.one = one;}}>
           <source src='../sounds/music/1.mp3' type="audio/mpeg" ></source>
         </audio>
-        <button className="btn btn-info" onClick={this.playAudio}>test sound</button>
+        <audio ref={(two) => {this.two = two;}}>
+          <source src='../sounds/music/2.mp3' type="audio/mpeg" ></source>
+        </audio>
+        <audio ref={(three) => {this.three = three;}}>
+          <source src='../sounds/music/3.mp3' type="audio/mpeg" ></source>
+        </audio>
+        <audio ref={(four) => {this.four = four;}}>
+          <source src='../sounds/music/4.mp3' type="audio/mpeg" ></source>
+        </audio>
+        <button className="btn btn-info" onClick={this.playSound}>test sound</button>
       </div>
 
     )
